614c37befda5e9dad3c3883707732bfccaf0a9bb
savasabd <savas1980@windowslive.com>
2021-10-30 21:22:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#print("merhaba python")
+#python
+#yemek-tabak
+#su-bardak
+#cerez- cerez tabak
+# sayılar
+#kelimeler
+#harfler
+#doğru yanlış
+#zaman
+# tam sayılar
+#ondalık sayılar
+#integer tamsayı
+#string kelime
+benimkelimem="Abdurrahman";# değişken
+print()
+benimsayim1=12
+benimsayim2=13
+sayi1=32;
+sayi2=6;
+sonuc=sayi1*sayi2;
+print(sonuc)
